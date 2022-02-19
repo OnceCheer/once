b011c12d1b2d359468ba20f797724eae1d391fa3
145239 <1452396512@qq.com>
2022-02-20 00:03:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>第一次  哦</div>
+<div v-for="item in 10" :key="item">{{item}}</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
